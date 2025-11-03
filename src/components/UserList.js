c419b3139fdba5e8f5ea2e9b4995aced77f520c7
Avantika10589 <127646223+Avantika10589@users.noreply.github.com>
2025-11-03 17:52:11 +0530

    Create UserList.js

---
+++ src/components/UserList.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            <Link to={`/users/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
